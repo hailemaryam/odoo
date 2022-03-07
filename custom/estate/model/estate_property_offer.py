3089f734d16b9657cd95b9ea4bd0c6288bc57070
hailemaryam <hailemaryammecca@gmail.com>
2022-03-07 19:29:21 +0300

    computed fields and onchange third exersice inverse done

--- custom/estate/model/estate_property_offer.py
+++ custom/estate/model/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date, datetime, timedelta
 
 
 class EstatePropertyOffer(models.Model):
@@ -10,5 +11,24 @@ class EstatePropertyOffer(models.Model):
         selection=[('Accepted', 'Accepted'), ('Refused', 'Refused')],
         copy=False
     )
+    validity = fields.Integer('Validity')
+    date_deadline = fields.Date('Dead Line', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
     partner_id = fields.Many2one('res.partner', string='Buyer', required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = datetime.now() + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                delta_days = record.date_deadline - record.create_date.date()
+                record.validity = delta_days.days
+            else:
+                delta_days = record.date_deadline - date.today()
+                record.validity = delta_days.days
