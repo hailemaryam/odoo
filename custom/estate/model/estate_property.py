c47316b5ff4054485b32aeee0330d6bba589030a
hailemaryam <hailemaryammecca@gmail.com>
2022-03-07 19:51:27 +0300

    computed fields and onchange third exersice onchange done

--- custom/estate/model/estate_property.py
+++ custom/estate/model/estate_property.py
@@ -37,6 +37,15 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offer')
 
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
+
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for records in self:
