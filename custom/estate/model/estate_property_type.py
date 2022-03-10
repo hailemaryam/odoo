431851e5354e2fd3176e72d4ebbf2a57214b9ed5
hailemaryam <hailemaryammecca@gmail.com>
2022-03-10 19:24:17 +0300

    sprinkler exercise four manual ordering done Notice There is default ordering on the xml file

--- custom/estate/model/estate_property_type.py
+++ custom/estate/model/estate_property_type.py
@@ -8,6 +8,7 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char('Name', required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id', string='Properties')
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
     _sql_constraints = [
         ('unique_tag', 'UNIQUE(name)', 'tag name should be unique.')
     ]
